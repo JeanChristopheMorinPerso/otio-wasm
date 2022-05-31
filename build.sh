92137c28d5cc16f1f3931229295b39ca025cbee9
Jean-Christophe Morin <jean_christophe_morin@hotmail.com>
2022-05-30 20:03:01 -0400

    fix

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 # docker run -ti --rm -v $(pwd):/src/otio-wasm pyodide/pyodide:0.20.0 bash
 
-cd pyodide
-# make
+repoPath="$1"
+if [[ repoPath == "" ]]; then
+    echo "You need to specify the path to the repo"
+    exit 1
+fi
+
+cd /src/pyodide
 
 mkdir /src/pyodide/packages/opentimelineio
 cd /src/pyodide/packages/opentimelineio
@@ -13,13 +18,15 @@ package:
   version: 0.15.0.dev1
 
 source:
-  url: "file:///src/otio-wasm/OpenTimelineIO-0.15.0.dev1.tar.gz"
-  sha256: '$(sha256sum /src/otio-wasm/OpenTimelineIO-0.15.0.dev1.tar.gz | cut -d' ' -f1)'
+  url: "file://'"$repoPath"'/OpenTimelineIO-0.15.0.dev1.tar.gz"
+  sha256: '$(sha256sum "$repoPath/OpenTimelineIO-0.15.0.dev1.tar.gz" | cut -d' ' -f1)'
   patches:
   - patches/patch.patch
 ' > meta.yaml
 
 mkdir /src/pyodide/packages/opentimelineio/patches
-cp /src/otio-wasm/patch.patch /src/pyodide/packages/opentimelineio/patches
+cp "$repoPath/patch.patch" /src/pyodide/packages/opentimelineio/patches/
+
+cat /src/pyodide/packages/opentimelineio/meta.yaml
 
 python -m pyodide_build buildpkg meta.yaml
